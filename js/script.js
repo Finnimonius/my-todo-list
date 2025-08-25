f6fdb966f27738ca4e7fb44c32680662c00c3279
Aleksandr Zykov <Finnimonius97@gmail.com>
2025-08-25 12:57:41 +0400

    Add modal and styles

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-const themeToggle = document.getElementById('theme-toggle');
+const themeToggle = document.getElementById("theme-toggle");
 const html = document.documentElement;
 
-// Проверяем сохраненную тему
-const savedTheme = localStorage.getItem('theme');
-
-if (savedTheme === 'dark') {
-    html.setAttribute('data-theme', 'dark');
-    themeToggle.checked = true;
+if (localStorage.getItem("theme") === "dark") {
+    html.setAttribute("data-theme", "dark")
+    themeToggle.checked = true
 }
 
-// Обработчик переключения темы
-themeToggle.addEventListener('change', function () {
+themeToggle.addEventListener("change", () => {
     if (themeToggle.checked) {
-        html.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        html.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
     } else {
-        html.removeAttribute('data-theme');
-        localStorage.setItem('theme', 'light');
+        html.removeAttribute("data-theme", "dark")
+        localStorage.setItem("theme", "light")
     }
-});
+}) 
+
+const dialog = document.getElementById("dialog");
+const noteBtn = document.getElementById("note-btn");
+
+noteBtn.addEventListener("click", () => {
+    dialog.showModal()
+})
